e5d73e3a519e3ab634f04a7098ed440a679a4ed8
naglaa77 <sama_hap@yahoo.com>
2023-03-21 23:09:38 +0100

    add fixture and facker data

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+// src/DataFixtures/ArticleFixtures.php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 20; $i++) {
+            $article = new Article();
+            $article->setTitle($faker->sentence);
+            $article->setDescription($faker->text);
+            $article->setCreatedAt($faker->dateTimeBetween('-1 years', 'now'));
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
